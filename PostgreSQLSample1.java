f2bf970dc731fb6a70115def310f66ec0ec616b5
Akio Yano <akio.yano@unleash-jp.com>
2022-06-11 15:34:29 +0900

    init.

---
+++ PostgreSQLSample1.java
@@ -0,0 +1,32 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PostgreSQLSample1 {
+
+    public static void main(String[] args) throws SQLException {
+
+        String hostname = "";
+        String dbname = "";
+        String username = "";
+        String password = "";
+        Connection conn = null;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection("jdbc:postgresql://" + hostname
+                    + ":5432/" + dbname, username, password);
+            System.out.println("Success!!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Check communication from Java to postgresql
+
+## 1.Install JBDC Driver
+
+https://jdbc.postgresql.org/download.html
+
+## 2.Run by command
+
+```
+javac -encoding utf-8 PostgreSQLSample1.java
+java -cp ".;postgresql-42.4.0.jar" PostgreSQLSample1
+```
